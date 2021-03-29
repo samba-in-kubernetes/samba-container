90edaaff5ba195d2d0be5e63776c4b2f1da52e20
Michael Adam <obnox@redhat.com>
2021-03-29 19:19:47 +0200

    tests: add scripts for creating and deleting domain member
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- tests/common.sh
+++ tests/common.sh
@@ -5,6 +5,10 @@ BASE_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
 
 AD_DEPLOYMENT_YAML="${SCRIPT_DIR}/files/samba-ad-server-deployment.yml"
 AD_DEPLOYMENT_NAME="samba-ad-server"
+MEMBER_POD_YAML="${SCRIPT_DIR}/files/samba-domain-member-pod.yml"
+MEMBER_POD_NAME="samba-dm"
+MEMBER_CM_NAME="samba-container-config"
+MEMBER_SECRET_NAME="ad-join-secret"
 
 KEEP=${KEEP:-0}
 
