e42bb52a8a624e37547ec39ce5f1f38c636d9f96
Michael Adam <obnox@redhat.com>
2021-03-29 19:13:53 +0200

    test:ad-server: deploy and test the member server
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- tests/test-samba-ad-server-kubernetes.sh
+++ tests/test-samba-ad-server-kubernetes.sh
@@ -10,6 +10,16 @@ kubectl exec "${podname}" -- samba-tool domain info 127.0.0.1
 [ $? -eq 0 ] || _error "Error listing domain info"
 echo
 
+source ${SCRIPT_DIR}/test-deploy-ad-member.sh
+
+kubectl exec "${podname}" -c "smb" -- smbclient -N -L 127.0.0.1
+kubectl exec "${podname}" -c "winbind" -- wbinfo -t
+# note: testing `wbinfo -t` from the smb container again
+# to make sure samba can communicate to winbindd
+kubectl exec "${podname}" -c "smb" -- wbinfo -t
+
+source ${SCRIPT_DIR}/test-remove-ad-member.sh
+
 source ${SCRIPT_DIR}/test-remove-ad-server.sh
 
 echo
