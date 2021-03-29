d754c7df1a3b4f29709c817d97dfb510d5801389
Michael Adam <obnox@redhat.com>
2021-03-29 19:19:47 +0200

    tests: add scripts for creating and deleting domain member
    Signed-off-by: Michael Adam <obnox@redhat.com>

---
+++ tests/test-deploy-ad-member.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd)"
+
+source "${SCRIPT_DIR}/common.sh"
+
+echo "Creating ad member pod..."
+ERROR_MSG=$(kubectl create -f "${MEMBER_POD_YAML}" 2>&1 1>/dev/null)
+if [ $? -ne 0 ] ; then
+	if [[ "${ERROR_MSG}" =~ "AlreadyExists" ]] ; then
+		echo "pod exists already. Continuing."
+	else
+		_error "Error creating member pod."
+	fi
+fi
+
+kubectl get pod
+
+podname="$(kubectl get pod | grep "${MEMBER_POD_NAME}" | awk '{ print $1 }')"
+[ $? -eq 0 ] || _error "Error getting podname"
+
+echo "Samba ad member pod is $podname"
+
+echo "waiting for pod to be in Running state"
+tries=0
+podstatus="none"
+until [ $tries -ge 120 ] || echo $podstatus | grep -q 'Running'; do
+	sleep 1
+	echo -n "."
+	tries=$(( tries + 1 ))
+	podstatus="$(kubectl get pod $podname -o go-template='{{.status.phase}}')"
+done
+echo
+kubectl get pod
+echo
+echo $podstatus | grep -q 'Running' || _error "Pod did not reach Running state"
+
+echo "waiting for samba to become reachable"
+tries=0
+rc=1
+while [ $tries -lt 120 ] && [ $rc -ne 0 ]; do
+	sleep 1
+	tries=$(( tries + 1 ))
+	kubectl exec "${podname}" -c "smb" -- smbclient -N -L 127.0.0.1 2>/dev/null 1>/dev/null
+	rc=$?
+	echo -n "."
+done
+echo
+[ $rc -eq 0 ] || _error "Error: samba ad did not become reachable"
+
+echo "member setup done"
---
+++ tests/test-remove-ad-member.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd)"
+
+source "${SCRIPT_DIR}/common.sh"
+
+if [ ${KEEP} -eq 1 ]; then
+	echo "keeping ad member pod (KEEP=1)"
+	exit 0
+fi
+
+echo "removing ad member pod..."
+kubectl delete pod "${MEMBER_POD_NAME}"
+[ $? -eq 0 ] || _error "Error deleting pod"
+echo
+
+kubectl delete cm "${MEMBER_CM_NAME}"
+[ $? -eq 0 ] || _error "Error deleting configmap"
+echo
+
+kubectl delete secret "${MEMBER_SECRET_NAME}"
+[ $? -eq 0 ] || _error "Error deleting secret"
+echo
+
+echo "ad member pod removed"
