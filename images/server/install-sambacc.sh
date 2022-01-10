22c87f5bf380bed21086f99eb6ace17ccce7ec8b
John Mulligan <jmulligan@redhat.com>
2022-01-10 14:25:02 -0500

    server: add new shell scripts to help build container image
    Our container build files currently have a fair bit of logic
    in them. It's better to maintain them as shell scripts that
    get called inside the container.
    
    Not used yet.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

---
+++ images/server/install-sambacc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+wheeldir="$1"
+container_json_file="$2"
+
+wheeldir=/tmp
+wheel="$(find "${wheeldir}" -type f -name 'sambacc-*.whl')" \
+
+if ! [ $(echo "$wheel" | wc -l) = 1 ]; then
+    echo "more than one wheel file found"
+    exit 1
+fi
+
+pip install "$wheel"
+rm -f "$wheel"
+
+if [ "$container_json_file" ]; then
+    ln -sf "$container_json_file" /etc/samba/container.json
+fi
