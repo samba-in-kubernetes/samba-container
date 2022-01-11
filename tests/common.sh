bcd585beb7ef5af197e764090494925c78117c5c
John Mulligan <jmulligan@redhat.com>
2022-01-11 13:31:56 -0500

    tests: fix issues found by shellcheck
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- tests/common.sh
+++ tests/common.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$(cd "$(dirname "${0}")" && pwd)"
-BASE_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
 
-AD_DEPLOYMENT_YAML="${SCRIPT_DIR}/files/samba-ad-server-deployment.yml"
-AD_DEPLOYMENT_NAME="samba-ad-server"
-MEMBER_POD_YAML="${SCRIPT_DIR}/files/samba-domain-member-pod.yml"
-MEMBER_POD_NAME="samba-dm"
-MEMBER_CM_NAME="samba-container-config"
-MEMBER_SECRET_NAME="ad-join-secret"
+export AD_DEPLOYMENT_YAML="${SCRIPT_DIR}/files/samba-ad-server-deployment.yml"
+export AD_DEPLOYMENT_NAME="samba-ad-server"
+export MEMBER_POD_YAML="${SCRIPT_DIR}/files/samba-domain-member-pod.yml"
+export MEMBER_POD_NAME="samba-dm"
+export MEMBER_CM_NAME="samba-container-config"
+export MEMBER_SECRET_NAME="ad-join-secret"
 
-KEEP=${KEEP:-0}
+export KEEP=${KEEP:-0}
 
 _error() {
 	echo "$@"
