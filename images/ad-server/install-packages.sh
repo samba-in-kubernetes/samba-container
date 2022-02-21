d145f2e51b09e072ec82dd7ccf5c583c1fbca37f
Anoop C S <anoopcs@samba.org>
2022-02-21 22:33:18 +0530

    ad-server: Add mechanism to install RPMs from non-standard repos
    We also add a special case for accepting nightly built Samba RPMs.

---
+++ images/ad-server/install-packages.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -ex
+
+get_custom_repo() {
+    url="$1"
+    fname="$(basename "$url")"
+    dest="/etc/yum.repos.d/${fname}"
+    dnf install --setopt=install_weak_deps=False -y curl
+    curl "$url" -o "$dest"
+}
+
+install_packages_from="$1"
+case "${install_packages_from}" in
+    samba-nightly)
+        get_custom_repo "http://artifacts.ci.centos.org/samba/pkgs/master/fedora/samba-nightly-master.repo"
+    ;;
+    custom-repo)
+        get_custom_repo "${INSTALL_CUSTOM_REPO}"
+    ;;
+esac
+
+dnf install --setopt=install_weak_deps=False -y \
+    samba-dc \
+    procps-ng \
+    /usr/bin/smbclient
+dnf clean all
+
+rm -rf /etc/samba/smb.conf
