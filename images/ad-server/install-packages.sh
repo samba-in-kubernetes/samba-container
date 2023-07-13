299872a7e11860b5d19919c70982932a7d9e98cc
Anoop C S <anoopcs@samba.org>
2023-07-13 12:33:07 +0530

    images/ad-server: Prepare for building CentOS based images
    Nightly builds of Samba on CentOS 9 contains AD DC components. Thus we
    could create images with those nightly builds based on CentOS. Therefore
    make changes accordingly in the install scripts.
    
    Signed-off-by: Anoop C S <anoopcs@samba.org>

--- images/ad-server/install-packages.sh
+++ images/ad-server/install-packages.sh
@@ -6,25 +6,35 @@ get_custom_repo() {
     url="$1"
     fname="$(basename "$url")"
     dest="/etc/yum.repos.d/${fname}"
-    dnf install --setopt=install_weak_deps=False -y curl
+    dnf install --setopt=install_weak_deps=False -y /usr/bin/curl
     curl -L "$url" -o "$dest"
 }
 
 install_packages_from="$1"
 samba_version_suffix="$2"
 install_custom_repo="$3"
+
+# shellcheck disable=SC1091
+OS_BASE="$(. /etc/os-release && echo "${ID}")"
+
 case "${install_packages_from}" in
     samba-nightly)
         # unset version suffix for nightly builds
         samba_version_suffix=""
-        get_custom_repo "https://artifacts.ci.centos.org/samba/pkgs/master/fedora/samba-nightly-master.repo"
+        get_custom_repo "https://artifacts.ci.centos.org/samba/pkgs/master/${OS_BASE}/samba-nightly-master.repo"
     ;;
     custom-repo)
         get_custom_repo "${install_custom_repo}"
     ;;
 esac
 
-dnf install --setopt=install_weak_deps=False -y \
+dnf_cmd=(dnf)
+if [[ "${OS_BASE}" = centos ]]; then
+    dnf install -y epel-next-release
+    dnf_cmd+=(--enablerepo=crb)
+fi
+
+packages=(\
     findutils \
     python-pip \
     python3-samba \
@@ -32,7 +42,10 @@ dnf install --setopt=install_weak_deps=False -y \
     tdb-tools \
     "samba-dc${samba_version_suffix}" \
     procps-ng \
-    /usr/bin/smbclient
+    /usr/bin/smbclient)
+"${dnf_cmd[@]}" \
+    install --setopt=install_weak_deps=False -y \
+    "${packages[@]}"
 dnf clean all
 
 rm -rf /etc/samba/smb.conf
