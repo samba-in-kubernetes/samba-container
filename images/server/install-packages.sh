f23bc1909ef01d1bc227853406e11a757804be0d
Anoop C S <anoopcs@samba.org>
2025-09-16 11:15:38 +0530

    images/server: Switch to Samba v4.23 for ceph20
    Signed-off-by: Anoop C S <anoopcs@samba.org>

--- images/server/install-packages.sh
+++ images/server/install-packages.sh
@@ -159,7 +159,7 @@ case "${install_packages_from}" in
         package_selection=${package_selection:-custom-devbuilds}
     ;;
     ceph20)
-        get_sig_samba_repo "4.22"
+        get_sig_samba_repo "4.23"
         get_distro_ceph_repo "tentacle"
         package_selection=${package_selection:-stable}
     ;;
